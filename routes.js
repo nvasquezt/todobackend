7df0e712772572087fde198c4b543046017c70e8
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-03-30 10:59:10 -0500

    fixed healthcheck

--- routes.js
+++ routes.js
@@ -6,9 +6,10 @@ const healthcheck = require('./api/healthcheck');
 const tasks = require('./api/tasks');
 const user= require('./api/user');
 const authLocal = require('./auth/local');
+const healthcheck = require('./api/healthcheck');
 
 function routes(app) {
-    app.use('/api/helloworld', helloWorld);
+    app.use('/api/healthcheck', healthcheck);
     app.use('/api/tasks', tasks);
     app.use('/api/user', user);
     //local auth routes
